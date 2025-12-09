32f6af5fc67be58ff1da9e1c4a4210804fb7c68c
TKaylorDev <teresa.kaylor@protonmail.com>
2025-12-09 10:15:05 -0800

    Update what-is-my-ip.php

--- code-samples/what-is-my-ip.php
+++ code-samples/what-is-my-ip.php
@@ -3,4 +3,3 @@ $ip = file_get_contents('https://api.ipify.org');
 echo '<div style="position:fixed;top:0;left:0;background:black;color:yellow;padding:20px;z-index:999999;font-size:20px;">
 SERVER IP: ' . $ip . '
 </div>';
-?>
